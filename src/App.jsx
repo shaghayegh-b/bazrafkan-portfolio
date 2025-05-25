4f9f8899833a2cc4624f15f89e07fcee2529f062
shaghayegh <bazrafkan.sh@gmail.com>
2025-05-25 12:53:54 +0330

    change data

--- src/App.jsx
+++ src/App.jsx
@@ -5,43 +5,8 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import Myproject from "./components/Myproject/Myproject";
-import img1 from "./assets/imag/24.jpg";
-import img2 from "./assets/imag/1.jpg";
-import portfolio from "./assets/imag/18.png";
+import { projects } from "./components/Myproject/data";
 export default function App() {
-  let projects = [
-    {
-        img:portfolio,
-      title: "project1",
-      text: "In a portfolio page, the goal is to showcase your skills, projects, experiences, and contact information in a way that allows visitors—such as potential employers or clients—to quickly gain a clear understanding of your abilities.",
-      mylink:"https://bazrafkan-portfolio.liara.run/"
-    },
-    {
-      img:img1,
-      title: "project2",
-      text:"This section of the portfolio is currently being designed and developed. Full details and project information will be available here soon. Thank you for your patience and support — please stay tuned.",
-    },
-    {
-      img:img2,
-      title: "project3",
-      text:"This section of the portfolio is currently being designed and developed. Full details and project information will be available here soon. Thank you for your patience and support — please stay tuned.",
-    },
-    {
-        img:img2,
-      title: "project4",
-      text:"This section of the portfolio is currently being designed and developed. Full details and project information will be available here soon. Thank you for your patience and support — please stay tuned.",
-    },
-    {
-        img:img1,
-      title: "project5",
-      text:"This section of the portfolio is currently being designed and developed. Full details and project information will be available here soon. Thank you for your patience and support — please stay tuned.",
-    },
-    {
-        img:img2,
-      title: "project6",
-      text:"This section of the portfolio is currently being designed and developed. Full details and project information will be available here soon. Thank you for your patience and support — please stay tuned.",
-    },
-  ];
   return (
     <div className="mycontainer">
       <div className="head">
