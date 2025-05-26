81ca4690926836b0abb52a4b7a81db695957780c
shaghayegh <bazrafkan.sh@gmail.com>
2025-05-26 18:38:28 +0330

    img

--- data.jsx
+++ data.jsx
@@ -1,38 +1,38 @@
 export const projects=[
     {
         "id":1,
-        "img":"./src/assets/imag/18.webp",
+        "img":"https://raw.githubusercontent.com/shaghayegh-b/bazrafkan-portfolio/refs/heads/main/src/assets/imag/18.webp",
       "title": "project1",
       "text": "In a portfolio page, the goal is to showcase your skills, projects, experiences, and contact information in a way that allows visitors—such as potential employers or clients—to quickly gain a clear understanding of your abilities.",
       "mylink":"https://bazrafkan-portfolio.liara.run/"
     },
     {
         "id":2,
-      "img": "./src/assets/imag/1.webp",
+      "img": "https://raw.githubusercontent.com/shaghayegh-b/bazrafkan-portfolio/refs/heads/main/src/assets/imag/1.webp",
       "title": "project2",
       "text":"This section of the portfolio is currently being designed and developed. Full details and project information will be available here soon. Thank you for your patience and support — please stay tuned."
     },
     {
         "id":3,
-      "img":"./src/assets/imag/24.webp",
+      "img":"https://raw.githubusercontent.com/shaghayegh-b/bazrafkan-portfolio/refs/heads/main/src/assets/imag/24.webp",
       "title": "project3",
       "text":"This section of the portfolio is currently being designed and developed. Full details and project information will be available here soon. Thank you for your patience and support — please stay tuned."
     },
     {
         "id":4,
-        "img": "./src/assets/imag/1.webp",
+        "img": "https://raw.githubusercontent.com/shaghayegh-b/bazrafkan-portfolio/refs/heads/main/src/assets/imag/1.webp",
       "title": "project4",
       "text":"This section of the portfolio is currently being designed and developed. Full details and project information will be available here soon. Thank you for your patience and support — please stay tuned."
     },
     {
         "id":5,
-        "img":"./src/assets/imag/24.webp",
+        "img":"https://raw.githubusercontent.com/shaghayegh-b/bazrafkan-portfolio/refs/heads/main/src/assets/imag/24.webp",
       "title": "project5",
       "text":"This section of the portfolio is currently being designed and developed. Full details and project information will be available here soon. Thank you for your patience and support — please stay tuned."
     },
     {
         "id":6,
-        "img": "./src/assets/imag/1.webp",
+        "img": "https://raw.githubusercontent.com/shaghayegh-b/bazrafkan-portfolio/refs/heads/main/src/assets/imag/1.webp",
       "title": "project6",
       "text":"This section of the portfolio is currently being designed and developed. Full details and project information will be available here soon. Thank you for your patience and support — please stay tuned."
     }
